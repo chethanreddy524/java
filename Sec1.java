89322fbc8425b43cebb1b249a1f10c70543e17fc
CHETHAN K P <chethanreddy524@gmail.com>
2025-09-01 11:45:57 +0530

    Add files via upload

---
+++ Sec1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Sec1 {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        int a=Sc.nextInt();
+        if(a>0){
+            System.out.println("the number is positive");
+        }
+        else{
+            System.out.println("the number is negative");
+        }
+
+
+    }
+
+    
+    
+}
