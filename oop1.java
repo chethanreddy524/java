0ce3efdbdfef72a34b9e1f6d11b37e9a22bc21d0
CHETHAN K P <chethanreddy524@gmail.com>
2025-09-16 15:14:22 +0530

    Add files via upload

---
+++ oop1.java
@@ -0,0 +1,59 @@
+/*public class oop1 {
+    public static void main(String[] args) {
+        int x = 15,y = 10;
+        System.out.println("x<y:"+(x<y));
+    }
+    
+}
+*/
+ /* 
+public class oop1 {
+    public static void main(String[] args) {
+        int x = 15,y = 10;
+        System.out.println("x>y:"+(x>y));
+    }
+    
+}
+*/
+ /* 
+public class oop1 {
+    public static void main(String[] args) {
+        int age = 25;
+        boolean hasLicense = true;
+        if (age > 18 && hasLicense) {
+            System.out.println("eligible to drive");
+
+        } 
+        else {
+            System.out.println("not eligible to drive");
+
+        }
+    }
+}
+*/
+
+/*public class oop1{
+    public static void main(String[] args) {
+        boolean isRaining = false;
+        if (!isRaining) {
+            System.out.println("without umbrella");
+
+        } 
+        else { 
+            System.out.println("with umbrella");
+        }
+    }
+}*/
+public class oop1 {
+    public static void main(String[] args) {
+        boolean isStudent = true;
+        boolean isTeacher = false;
+
+        if (isStudent || isTeacher){
+            System.out.println("you can enter the campus");
+
+        } else {
+            System.out.println("access denied");
+        }
+    }
+}
